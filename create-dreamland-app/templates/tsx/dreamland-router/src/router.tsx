57dc27df025a8afdf61aa8933eb475b9d43c398c
MotorTruck1221 <motortruck1221@protonmail.com>
2024-06-21 02:42:04 -0600

    CDA: add better looking template
    (JSX, default only right now)

--- create-dreamland-app/templates/tsx/dreamland-router/src/router.tsx
+++ create-dreamland-app/templates/tsx/dreamland-router/src/router.tsx
@@ -1,7 +1,9 @@
 import { Redirect, Route } from 'dreamland-router';
 import Home from './routes/home';
 export const Router = (
+    //@ts-ignore temp fix
     <Route path="/">
+        {/* @ts-ignore temp fix */}
         <Route path="" show={<Home />} />
     </Route>
 ).$;
