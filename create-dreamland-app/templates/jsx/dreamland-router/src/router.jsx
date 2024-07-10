13baaa9e79c1e0deae081c23f89095d92bcb6c69
MotorTruck1221 <motortruck1221@protonmail.com>
2024-07-10 00:29:04 -0600

    .

--- create-dreamland-app/templates/jsx/dreamland-router/src/router.jsx
+++ create-dreamland-app/templates/jsx/dreamland-router/src/router.jsx
@@ -1,7 +1,7 @@
 import { Route } from 'dreamland-router';
 import Home from './routes/home.jsx';
 export const Router = (
-    <Route path="/">
-        <Route path="" show={<Home />} />
+    <Route>
+        <Route path="/" show={<Home />} />
     </Route>
 ).$;
--- create-dreamland-app/templates/tsx/dreamland-router/src/router.tsx
+++ create-dreamland-app/templates/tsx/dreamland-router/src/router.tsx
@@ -1,9 +1,7 @@
 import { Route } from 'dreamland-router';
 import Home from './routes/home';
 export const Router = (
-    //@ts-ignore temp fix
     <Route path="/">
-        {/* @ts-ignore temp fix */}
         <Route path="" show={<Home />} />
     </Route>
 ).$;
