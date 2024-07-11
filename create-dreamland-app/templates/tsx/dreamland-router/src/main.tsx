ca1def8c62abdc2f5f1d6b04465a567ee4964c04
MotorTruck1221 <motortruck1221@protonmail.com>
2024-07-11 01:25:40 -0600

    CDA: bump DL

--- create-dreamland-app/templates/tsx/dreamland-router/src/main.tsx
+++ create-dreamland-app/templates/tsx/dreamland-router/src/main.tsx
@@ -5,9 +5,7 @@ import Home from './routes/home';
 //base styles
 import './index.css';
 let router = new Router(
-    //@ts-ignore TEMP FIX FOR NOW
     <Route>
-        {/* @ts-ignore TEMP FIX FOR NOW */}
         <Route path="" show={<Home />} />
     </Route>
 ).mount(document.getElementById('app')!);
